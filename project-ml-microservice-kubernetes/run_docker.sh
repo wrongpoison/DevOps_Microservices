ab000716c924e199cdf9680926f236b81518b9a0
Margaux <mbavlsik@gmail.com>
2019-11-28 09:05:26 -0600

    update dockerfile and makefile

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=api . 
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 80:5001 api
