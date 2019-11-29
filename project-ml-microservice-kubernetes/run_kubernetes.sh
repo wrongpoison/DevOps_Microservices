cd79efa311e71a75d0747cb5f8190bff71b5ec7f
Margaux <mbavlsik@gmail.com>
2019-11-29 15:18:03 -0600

    add circleci

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="wrongpoison/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api --image=$dockerpath --port=80 --labels app=api --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward api 8000:80
